368f793a5acfd1f5e4a0289c77f040512abdd26a
theri <therigasmazurets@gmail.com>
2023-12-10 19:59:00 +0200

    update

--- app/src/main/java/com/example/weatherapp/app/ui/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/app/ui/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.weatherapp.app.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.weatherapp.R
+import com.example.weatherapp.app.ui.fragments.TodayFragment
 import com.example.weatherapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +11,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
+
+        supportFragmentManager.beginTransaction()
+            .replace(binding.flFragment.id, TodayFragment())
+            .addToBackStack(null)
+            .commit()
 
 
     }
